6ddc4f42bff36c343b0d1c348e0147cc873b0611
MVDVV <123770336+MVDVV@users.noreply.github.com>
2025-12-12 01:30:05 +0300

    Update records.py

--- kasparromain/schemas/records.py
+++ kasparromain/schemas/records.py
@@ -1,4 +1,6 @@
 # Pydantics schema for unified records
+# canonical_id's have been modified as requested
+
 from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Optional
@@ -20,9 +22,9 @@ class CoinGeckoItem(BaseModel):
     @property
     def canonical_id(self) -> str:
 
-        """Unique row key based on source + coin id + timestamp."""
+        """coin id+ source as unique key. just in case paparika has different ids"""
 
-        return f"coingecko:{self.id}:{int(self.ts.timestamp())}"
+        return f"coingecko:{self.id}"
 
     @property
     def normalized_value(self) -> float:
@@ -36,24 +38,24 @@ class CoinGeckoItem(BaseModel):
         return self.name or self.id or "unknown"
     
 class PaprikaItem(BaseModel):
-    id: str                     #"btc-bitcoin"
-    symbol: Optional[str]       #"btc"
-    name: Optional[str]         #"Bitcoin"
+    id: str                     
+    symbol: Optional[str]       
+    name: Optional[str]         
 
     price_usd: Optional[float]
     market_cap_usd: Optional[float]
     volume_24h_usd: Optional[float]
     percent_change_1h: Optional[float]
     last_updated : Optional[datetime]
-    raw_payload: dict           #raw JSON
+    raw_payload: dict          
     ts: datetime = Field(default_factory=datetime.utcnow)
 
     @property
     def canonical_id(self) -> str:
 
-        """Unique row key based on source + coin id + timestamp."""
+        """new Unique row key based on source + coin id"""
 
-        return f"paprika:{self.id}:{int(self.ts.timestamp())}"
+        return f"paprika:{self.id}"
 
     @property
     def normalized_value(self) -> float:
