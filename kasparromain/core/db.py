3b7b364ca068de84fecbba024ae577c9a9bbc3d2
MVDVV <123770336+MVDVV@users.noreply.github.com>
2025-12-12 01:26:14 +0300

    updated to accommodate pooling

--- kasparromain/core/db.py
+++ kasparromain/core/db.py
@@ -1,6 +1,7 @@
 # core/db.py
 import os
 import psycopg2
+import psycopg2.pool
 from pathlib import Path
 from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
@@ -8,15 +9,17 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL") or os.getenv("DATABASE_URL_LOCAL") or "postgresql://app:password@postgres:5432/appdb"
 
-_conn = None
+connection_pool =  psycopg2.pool.SimpleConnectionPool(minconn = 4 ,maxconn = 4, dsn = DATABASE_URL)
 
 def get_db_conn():
-    global _conn
-    if _conn is None or _conn.closed:
-        _conn = psycopg2.connect(DATABASE_URL)
-    return _conn
+    conn = connection_pool.getconn() 
+    return conn
+
+def release_db_conn(conn):
+    connection_pool.putconn(conn)
 
 def ensure_tables():
+    # If you prefer to run init_db.sql from compose, this is optional.
     BASE_DIR = Path(__file__).resolve().parents[0]
     SQL_FILE = BASE_DIR / "init_db.sql"
     SQL = SQL_FILE.read_text()
@@ -25,3 +28,4 @@ def ensure_tables():
     cur.execute(SQL)
     conn.commit()
     cur.close()
+    release_db_conn(conn) # release the connection back to the pool
